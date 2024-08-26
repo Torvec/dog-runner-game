564f62ee6570621f22fde65c42be71b2b4280380
Edward Vonschondorf <edvonschondorf@gmail.com>
2024-08-25 22:09:31 -0700

    Refactor core game class to split it apart to the scenes that need it and move what is left main js, and make other miscellaneouse changes

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import Game from "./lib/Game.js";
+import { InputHandler } from "./lib/InputHandler.js";
+import { SceneManager } from "./lib/SceneManager.js";
 
 window.addEventListener("load", () => {
   const canvas = document.getElementById("canvas1");
@@ -6,8 +7,28 @@ window.addEventListener("load", () => {
   canvas.width = 1200;
   canvas.height = 500;
 
+  class Game {
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.width = this.canvas.width;
+      this.height = this.canvas.height;
+      this.input = new InputHandler(this);
+      this.sceneManager = new SceneManager(this);
+      this.currentScene = this.sceneManager.setScene("START_MENU");
+    }
+    update(deltaTime) {
+      this.sceneManager.update(deltaTime, this.input);
+    }
+    draw(context) {
+      this.sceneManager.draw(context);
+    }
+  }
+
   const game = new Game(canvas);
+  console.log(game);
+
   let previousTimestamp = 0;
+  
   function animate(timeStamp) {
     const deltaTime = timeStamp - previousTimestamp;
     previousTimestamp = timeStamp;
