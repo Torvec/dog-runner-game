a1da1db11ec063ad8b84a628de472473455c0872
Edward Vonschondorf <edvonschondorf@gmail.com>
2024-08-31 10:26:48 -0700

    Fixed collision errors

--- main.js
+++ main.js
@@ -9,39 +9,39 @@ import {
   Credits,
 } from "./lib/scenes.js";
 
+class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.input = new InputHandler(this);
+    this.scenes = {
+      START_MENU: StartMenu,
+      LEVEL_ONE: LevelOne,
+      LEVEL_TWO: LevelTwo,
+      LEVEL_THREE: LevelThree,
+      GAME_COMPLETE: GameComplete,
+      SCOREBOARD: Scoreboard,
+      CREDITS: Credits,
+    };
+    this.currentScene = this.setScene("START_MENU");
+  }
+  setScene(sceneName) {
+    const SceneClass = this.scenes[sceneName];
+    this.currentScene = new SceneClass(this);
+    return this.currentScene;
+  }
+  render(deltaTime, context) {
+    this.currentScene.update(deltaTime);
+    this.currentScene.draw(context);
+  }
+}
+
 window.addEventListener("load", () => {
   const canvas = document.getElementById("canvas1");
   const context = canvas.getContext("2d");
   canvas.width = 1200;
   canvas.height = 500;
-
-  class Game {
-    constructor(canvas) {
-      this.canvas = canvas;
-      this.width = this.canvas.width;
-      this.height = this.canvas.height;
-      this.input = new InputHandler(this);
-      this.scenes = {
-        START_MENU: StartMenu,
-        LEVEL_ONE: LevelOne,
-        LEVEL_TWO: LevelTwo,
-        LEVEL_THREE: LevelThree,
-        GAME_COMPLETE: GameComplete,
-        SCOREBOARD: Scoreboard,
-        CREDITS: Credits,
-      };
-      this.currentScene = this.setScene("START_MENU");
-    }
-    setScene(sceneName) {
-      const SceneClass = this.scenes[sceneName];
-      this.currentScene = new SceneClass(this);
-      return this.currentScene;
-    }
-    render(deltaTime, context) {
-      this.currentScene.update(deltaTime);
-      this.currentScene.draw(context);
-    }
-  }
 
   const game = new Game(canvas);
 
